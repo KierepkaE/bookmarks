bae080c2826d0e83a16e75ba926ce12750983982
kierepkae <kierepkaedyta@gmail.com>
2019-08-23 10:11:22 +0100

    displaying bookmarks on /bookmarks

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require_relative './lib/Bookmark.rb'
 
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Bookmark Manager'
+    erb:'main/index'
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+     erb:'bookmarks/index'
   end
 
   run! if app_file == $0
