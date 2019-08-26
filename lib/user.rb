fc1a2b778ba4004fce7682deb09381e5c0874090
kierepkae <kierepkaedyta@gmail.com>
2019-08-26 12:07:55 +0100

    user can not log in when email is incorrect

--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,7 @@ require 'bcrypt'
 require_relative './database_connection.rb'
 class User
   attr_reader :id, :email
+
 def initialize(id:, email:)
   @id= id
   @email = email
@@ -17,5 +18,11 @@ end
   def self.find(user_id:)
     result = DatabaseConnection.query("SELECT * FROM users WHERE id = #{user_id};")
     User.new(id: result[0]['id'], email: result[0]['email'])
+  end
+
+  def self.authenticate(email:, password:)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
+    return unless result.any?
+    User.new(id: result[0]['id'], email:result[0]['email'])
   end
 end
